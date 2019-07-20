e5d687ecd583ada2053362a27c85ae49ac86d516
manish kumar jha <mkmanish1993@gmail.com>
2019-07-21 00:25:58 +0530

    Fixed project

---
+++ HeadySAT/Script.js
@@ -0,0 +1,43 @@
+﻿
+var headyApp = angular.module("headyApp", ["ngRoute"]).config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when("/details", {
+            templateUrl: "/Templates/details.htm",
+            controller: "detailsController"
+        })
+        .when("/description", {
+            templateUrl: "Templates/description_1.htm",
+            controller: "descriptionController"
+        })
+        .when("/reviews", {
+            templateUrl: "/Templates/reviews.htm",
+            controller: "reviewsController"
+        })
+        .when("/related", {
+            templateUrl: "Templates/related.htm",
+            controller: "relatedController"
+        }).otherwise({
+            redirectTo: "/details"
+        });
+    //    $locationProvider.html5mode(true);
+
+}]).controller("descriptionController",function($scope) {
+  $scope.message="description";
+});
+ angular.module("headyApp").controller("detailsController", function ($scope) {
+    $scope.message = "I love Paris";
+});
+// angular.module("headyApp").controller("descriptionController", function ($scope) {
+  //  $scope.message = "I love Paris";
+//});
+headyApp.controller("reviewsController", function ($scope) {
+    $scope.message = "Reviews Page";
+});
+headyApp.controller("relatedController", function ($scope) {
+    $scope.message = "Related Page";
+});
+
+//var changeText = function(id) {
+//    var getText = document.getElementById(id).innerText;
+//    document.getElementById("verticalText").innerText = getText;
+//}
---
+++ README.md
@@ -0,0 +1,9 @@
+# HeadySAT
+This is an SAT for Heady Technologies.
+This project was an attempt to make a small website with SPA feature along with the responsive UI.
+1) SPA was accomplished using AngularJS 1.6.X
+2) CSS Grids with media queries were used to create the responsive UI (desktop and mobile)
+
+Thanks.
+
+Ankit Anand
